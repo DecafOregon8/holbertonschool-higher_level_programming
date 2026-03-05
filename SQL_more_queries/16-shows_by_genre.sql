7e612dbfd573dae5c4d16619f8e6e4c08efa5ef7
root <root@ip-10-42-172-203.ec2.internal>
2026-03-05 03:24:16 +0000

    List all shows with genres using LEFT JOIN

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- 16-shows_by_genre.sql
+-- Lists all shows and all genres linked to that show
+-- Display: tv_shows.title - tv_genres.name (NULL if no genre)
+-- Sorted by show title and genre name in ascending order
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
