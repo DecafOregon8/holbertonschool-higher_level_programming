d142e4cc62c27d2ecb73e9c2f213cd265602b735
root <root@ip-10-42-172-203.ec2.internal>
2026-03-05 03:07:43 +0000

    List cities with states using INNER JOIN

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,9 @@
-# Verificar los datos primero
-echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
-echo 'SELECT * FROM cities;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- 9-cities_by_state_join.sql
+-- Lists all cities with their corresponding state names
+-- Display: cities.id - cities.name - states.name
+-- Sorted by cities.id in ascending order
 
-# Ejecutar el script
-cat 9-cities_by_state_join.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
