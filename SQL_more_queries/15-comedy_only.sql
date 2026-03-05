8c7b792e272d55edfe179043246befe421aae807
root <root@ip-10-42-172-203.ec2.internal>
2026-03-05 03:23:03 +0000

    List all Comedy shows using JOIN with genre filter

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- 15-comedy_only.sql
+-- Lists all Comedy shows in the database
+-- Display: tv_shows.title
+-- Sorted by show title in ascending order
+
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
