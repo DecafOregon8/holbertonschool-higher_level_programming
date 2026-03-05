ad9d2f8d5281a3ff1f6c9625ca53059d1e4cce53
root <root@ip-10-42-172-203.ec2.internal>
2026-03-05 03:18:27 +0000

    Count shows by genre using GROUP BY and COUNT

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- 13-count_shows_by_genre.sql
+-- Lists all genres and the number of shows linked to each
+-- Display: genre - number_of_shows
+-- Excludes genres with no shows
+-- Sorted by number_of_shows in descending order
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+ORDER BY number_of_shows DESC;
