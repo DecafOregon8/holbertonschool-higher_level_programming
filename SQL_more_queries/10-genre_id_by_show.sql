85daed2ff0dab4975b119eb418779ad16166ca96
root <root@ip-10-42-172-203.ec2.internal>
2026-03-05 03:10:32 +0000

    List shows with genres using INNER JOIN

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- 10-genre_id_by_show.sql
+-- Lists all shows with at least one genre linked
+-- Display: tv_shows.title - tv_show_genres.genre_id
+-- Sorted by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
